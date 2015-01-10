42b2f2502a3c749d34548907c84b44ef64b13a9d
Leo Xuzhang Lin <xuzhang@umass.edu>
2015-01-10 15:16:20 -0800

    Speech and tts setup. Both works

---
+++ app/src/main/java/com/hackthedrive/siride/MainActivity.java
@@ -0,0 +1,78 @@
+package com.hackthedrive.siride;
+
+import java.util.ArrayList;
+import java.util.Locale;
+
+import android.util.Log;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.speech.RecognizerIntent;
+import android.speech.tts.TextToSpeech;
+import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends Activity implements OnClickListener, TextToSpeech.OnInitListener{
+
+    protected static final int REQUEST_OK = 1;
+
+    // Log tag of the class
+    private static final String TAG = "MainActivity Log";
+
+    private TextToSpeech tts;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        findViewById(R.id.button1).setOnClickListener(this);
+        tts = new TextToSpeech(this, this);
+    }
+
+    @Override
+    public void onInit(int status) {
+        if (status == TextToSpeech.SUCCESS) {
+            tts.setLanguage(Locale.US);
+
+        } else {
+            Log.e("TTS", "Initialization failed");
+        }
+
+    }
+
+    private void say(String text){
+        tts.speak(text, TextToSpeech.QUEUE_FLUSH, null);
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        Intent i = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        i.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, "en-US");
+        try {
+            startActivityForResult(i, REQUEST_OK);
+        } catch (Exception e) {
+            Toast.makeText(this, "Error initializing speech to text engine.", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode==REQUEST_OK  && resultCode==RESULT_OK) {
+            ArrayList<String> thingsYouSaid = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+            ((TextView)findViewById(R.id.text1)).setText(thingsYouSaid.get(0));
+            say(thingsYouSaid.get(0));
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+}
